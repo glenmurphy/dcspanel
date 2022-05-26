1a4b5291416cfc8fe69191c41cacfe3c917f56af
Glen Murphy <glen@glenmurphy.com>
2022-05-27 09:48:01 +1000

    fix displays not being created

--- dist/controls/create.js
+++ dist/controls/create.js
@@ -5,14 +5,12 @@ import Display from "./display.js";
 import Control from "./control.js";
 
 export default function createControl(bios, control_config, control_scheme, update_handler) {
-  switch (control_scheme.physical_variant) {
-    case 'rocker_switch':
-      return new Rocker(bios, control_config, control_scheme, update_handler);
-    case 'push_button':
-      return new Button(bios, control_config, control_scheme, update_handler);
-    case 'display':
-      return new Display(bios, control_config, control_scheme, update_handler);
-    default:
-      return new Control(bios, control_config, control_scheme, update_handler);
-  }
+  if (control_scheme.physical_variant == 'rocker_switch')
+    return new Rocker(bios, control_config, control_scheme, update_handler);
+  else if (control_scheme.physical_variant == 'push_button')
+    return new Button(bios, control_config, control_scheme, update_handler);
+  else if (control_scheme.control_type == 'display')
+    return new Display(bios, control_config, control_scheme, update_handler);
+
+  return new Control(bios, control_config, control_scheme, update_handler);
 }
